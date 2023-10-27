f1b8ba9d018629c6b5a4a99bac1d283afc9688db
j9rely <145072305+j9rely@users.noreply.github.com>
2023-10-27 14:39:20 -0700

    syntax
    script

---
+++ ScriptingProjectP7Bueno/Assets/Scripts/Syntax.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Syntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this line is there to tell me the x position of my object
+
+        /*Hi There!
+         * this is two lines!
+         * */
+        Debug.Log(transform.position.x);
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
